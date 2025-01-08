fbf9093ae79c36d12c590a8baf101539ce3d66b6
fengjiran <fengjiran@foxmail.com>
2025-01-08 18:30:11 +0800

    add self attention module

--- model.py
+++ model.py
@@ -27,9 +27,56 @@ class LayerNorm(nn.Module):
         super(LayerNorm, self).__init__()
         self.gamma = nn.Parameter(torch.ones(ndim))
         self.beta = nn.Parameter(torch.zeros(ndim)) if bias else None
+        self.gelu = nn.GELU()
 
     def forward(self, x) -> torch.Tensor:
         return F.layer_norm(x, self.gamma.shape, self.gamma, self.beta, 1e-5)
+
+
+class CausalSelfAttention(nn.Module):
+    def __init__(self, config: GPTConfig):
+        super(CausalSelfAttention, self).__init__()
+        assert config.n_embd % config.n_head == 0
+
+        # input projection
+        self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd, bias=config.bias)
+
+        # output projection
+        self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias=config.bias)
+
+        self.attn_dropout = nn.Dropout(config.dropout)
+        self.residual_dropout = nn.Dropout(config.dropout)
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
+        self.dropout = nn.Dropout(config.dropout)
+        self.flash = hasattr(torch.nn.functional, "scaled_dot_product_attention")
+        if not self.flash:
+            print("WARNING: using slow attention. Flash Attention requires PyTorch >= 2.0")
+            # causal mask to ensure that attention is only applied to the left in the input sequence
+            self.register_buffer("bias", torch.tril(torch.ones(config.block_size, config.block_size))
+                                 .view(1, 1, config.block_size, config.block_size))
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        # batch size, sequence length, embedding dimensionality (n_embd)
+        B, N, C = x.size()
+
+        # calculate query, key, values for all heads in batch and move head forward to be the batch dim
+        query, key, value = self.c_attn(x).split(self.n_embd, dim = 2)
+
+
+class MLP(nn.Module):
+    def __init__(self, config: GPTConfig):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(config.n_embd, 4 * config.n_embd, bias=config.bias)
+        self.fc2 = nn.Linear(4 * config.n_embd, config.n_embd, bias=config.bias)
+        self.gelu = nn.GELU()
+        self.dropout = nn.Dropout(config.dropout)
+
+    def forward(self, x) -> torch.Tensor:
+        x = self.fc1(x)
+        x = self.gelu(x)
+        x = self.fc2(x)
+        return self.dropout(x)
 
 
 if __name__ == "__main__":
@@ -38,8 +85,11 @@ if __name__ == "__main__":
     # layer_norm = nn.LayerNorm([C, H, W])
     # output = layer_norm(input)
 
-    b, s, d = 10, 5, 512
+    b, s, d = 10, 5, 64 * 3
     input = torch.randn(b, s, d)
     layer_norm = LayerNorm(d, True)
     output = layer_norm(input)
+    assert output.is_contiguous()
+
+    q, k, v = output.split(64, dim=-1)
     print('done')
